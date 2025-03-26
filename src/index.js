86760ca15017648d9f624a5a024a3b2ce111f1e2
LeonardoN-Aragao <Leonardonunesaragao@gmail.com>
2025-03-26 19:03:57 -0300

    Add route and NotFound page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router";
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home'
+import About from './pages/About'
+import ContactMe from './pages/ContactMe'
+import NotFound from './pages/Notfound'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route index element={<Home />} />
-      <Route index element={<Info />} />
-      <Route index element={<ContactMe />} />
-      <Route index element={<NotFound />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contactMe" element={<ContactMe />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
+//      
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,10 @@
+
+const style = {
+    textAlign: 'center'
+}
+
+export default function NotFound() {
+    return (
+        <h1 style={style}>Sorry, Not Found.</h1>
+    )
+}
