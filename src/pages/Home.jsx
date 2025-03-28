d0fbcd70d65b096e2ee60d4b1f173e566221f3cc
LeonardoN-Aragao <Leonardonunesaragao@gmail.com>
2025-03-27 23:15:15 -0300

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,9 +12,8 @@ export default function Home () {
             <main>
                <h2>Hi, good to see you here!</h2>
                 <p>I'm Leonardo, a computer scientist, aka. Software Engeenier, FullStack Developer... but you can call me Leo.</p>
-                <p>Some of my projects and Experiencies are show above in a diferent way. Really didn't found any simple way to do it, so I plan to create a simple timeline component that doesn't need the last cpu generation to be render.</p>
+                <p>Some of my projects and Experiencies are show above in a diferent way. Really didn't found any simple way to do it, so I plan to create a simple timeline component that doesn't need much to be render.</p>
                 <p style={{color: 'red'}}>This page still a work in progress... (side projects while working can be really hard and demanding)</p>
-                <p style={{color: 'red'}}>Promise to finish this timeline component someday</p>
                 <Timeline />
             </main> 
         </div>
