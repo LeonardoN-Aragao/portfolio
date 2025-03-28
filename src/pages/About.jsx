42f5f9d7c3791821a1e159e57e37d6d51064ecfd
LeonardoN-Aragao <Leonardonunesaragao@gmail.com>
2025-03-27 23:16:09 -0300

    Add more text to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer"
 import Header from '../components/Header'
+import styled from 'styled-components'
 
 const image = require ('../assets/essa.png')
 
@@ -10,8 +11,16 @@ export default function About(){
             <div style={style.container}>
                 <img style={style.image} src={image} alt="me" width="175px" height="175"></img>
                 <h1> About me</h1>
+                
                 <h2> Resume </h2>
-                <p>FullStack Developer focus in React and Java</p>
+                <p>Leonardo Nunes Aragão, 27 years old</p>
+                <p>Married</p>
+                <p>FullStack Developer with focus in React and Java</p>
+                <p>Currently working at <Link href="https://compass.uol/en/home/">Compass</Link></p>
+                <p>My Degree Conclusion Work was <b>Using Data Warehouse to Analyse Energy Consumption in Brazil</b> it used python, sqlite and Data Science techniques to work with Big Data.</p>
+                
+                <h2>Skills</h2>
+                    
                 <h2> Education</h2>
                 <h3>Computer Science - Bachelor's Degree</h3>
                 <p>Universidade Federal De Juiz de Fora</p>
@@ -27,6 +36,29 @@ const style = {
         color: 'white'
     },
     image:{
+        //position: 'relative',
         clipPath: 'circle()'
     }
 }
+
+const Link = styled.a`
+  text-decoration: none;
+  color: lightSkyBlue;
+  margin-right: 1%;
+
+  &:visited {
+    text-decoration: none;
+    color: lightSkyBlue;
+  }
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+  }
+
+  &:hover,
+  &:active {
+    text-decoration: none;
+    color: gray;
+  }
+`
