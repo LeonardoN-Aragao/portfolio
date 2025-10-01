6b82e5f266c8962fd5a6c11312406e0aaa1de784
LeonardoN-Aragao <Leonardonunesaragao@gmail.com>
2025-10-01 11:36:54 -0300

    add projects image folder, fix typo & add new item in Timeline

--- src/itemList.jsx
+++ src/itemList.jsx
@@ -1,8 +1,31 @@
-const folder = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tZAgNczQdTrhHOclNnjwtQHaHa%26pid%3DApi&f=1&ipt=de3243a41745c91624a3ff37b14ee984011ff2b7f1d8f5bfecad920e768f1189&ipo=images"
+import ultracenter from './assets/projects/clinica_ultracenter.png'
+import folder from './assets/projects/folder.jpeg'
+import bancoBrasil from './assets/projects/banco_brasil.jpeg'
+import qsaude from './assets/projects/qsaude.jpeg'
+import calculator from './assets/projects/calculator.jpg'
+import vivoMoney from './assets/projects/vivo_money.jpeg'
 
 export const itens = [
+    // {
+    //     date: 'Aug - Out 2025',
+    //     title:'Vivo Worker\'s Credit',
+    //     name: "Fintech Vivo - Payroll Loan",
+    //     description: '',
+    //     keywords: [ 'Java', 'Spring Boot', 'Back-End' ],
+    //     image: folder,
+    //     link: 'unavailable'
+    // },
     {
-        date: 'Now',
+        date: 'Jun - Jul 2025',
+        title:'UltraCenter',
+        name: "UltraCenter website",
+        description: '',
+        keywords: [ 'WordPress', 'Php', 'FullStack' ],
+        image: ultracenter,
+        link: 'https://clinicaultracenter.com.br/'
+    },
+    {
+        date: '27 Mar 2025',
         title:'This Site',
         name: "Leo's Portfolio",
         description: 'Very useful site to show a little about what I know.',
@@ -19,41 +42,49 @@ export const itens = [
         image: folder,    
         link: 'https://github.com/LeonardoN-Aragao/chdir'
     },
-    
+    // {
+    //     date: '2024 - 2026',
+    //     title:'Vivo Money 2.0',
+    //     name: 'Fintech Vivo - Personal Loan',
+    //     description: 'Mobile Appication version of Vivo Personal loan - Integrated inside Vivo App',
+    //     keywords: [ 'SpringBoot', 'TypeScript', 'FullStack' ],
+    //     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5akqoMKQ-6blqnSRGjMIHwHaEK%26pid%3DApi&f=1&ipt=b15af798ae358858d47050d01f497243040a54a79b06e669b27eac7da5e62942&ipo=images',
+    //     link: 'https://www.vivomoney.com.br/'
+    // },
     {
         date: '2023 - 2025',
-        title:'Vivo Money',
+        title:'Vivo Money 1.0',
         name: 'Fintech Vivo - Personal Loan',
-        description: '',
+        description: 'Web Vivo Personal loan',
         keywords: [ 'SpringBoot', 'TypeScript', 'FullStack' ],
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5akqoMKQ-6blqnSRGjMIHwHaEK%26pid%3DApi&f=1&ipt=b15af798ae358858d47050d01f497243040a54a79b06e669b27eac7da5e62942&ipo=images',
+        image: vivoMoney,
         link: 'https://www.vivomoney.com.br/'
     },
     {
-        date: 'Set 2021',
+        date: 'Jul - Set 2021',
         title:'Qsaúde',
         name: 'Qsaúde website and system',
         description: 'Description',
         keywords: [ 'TypeScript', 'Java','Spring Boot', 'FullStack' ],
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.TVPQ7Tm6rovWKGtpY0xNgwHaEJ%26pid%3DApi&f=1&ipt=3c7afa96ef57b07e22d40ef26727d0debec076f44516298a67e96dcc726a140b&ipo=images',
+        image: qsaude,
         link: 'https://qsaudesite.com.br/'
     },
     {
-        date: 'April 2021',
+        date: 'Apr 2021',
         title:'Federal Subsidy',
         name: "Banco do Brasil - Federal Agricultural Subsidy project",
         description: 'My first great project, were used very strict procedures to ensure quality and security of the system.',
         keywords: [ 'Java', 'SpringBoot', 'Back-End' ],
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.7Glzg8ZndW33VB9yZ-1ehAHaE3%26pid%3DApi&f=1&ipt=8daab75756b0284f5ec533e43a8d84b0e81b661dad78f521dd6a378871e16003&ipo=images',
+        image: bancoBrasil,
         link: 'https://www.bb.com.br/site/agronegocios/'
     },
     {
-        date: 'April 2014',
+        date: 'Apr 2014',
         title:'My first project',
         name: 'Calculator',
         description: 'Something lead me to do this, I was learning my first language C#',
         keywords: [ 'C#', 'VB', 'Windows' ],
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F0qSbkFaTj-8%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=8f5e13e21d261ebf35c35d4854ea856e6f93658e9e37c4b091fa1b9c3846267f',
+        image: calculator,
         link: ''
     }
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home () {
             <main>
                <h2>Hi, good to have you here!</h2>
                 <p>I'm Leonardo, but call me Leo. </p>
-                <p>A computer scientist, aka. Software Engeenier, FullStack Developer... </p>
+                <p>A computer scientist, aka. Software Engineer, FullStack Developer... </p>
                 <p>Some of my projects and Experiencies are show above in a diferent way. Really didn't found any simple way to do it, so I plan to create a simple timeline component that doesn't need much to be render.</p>
                 <p style={{color: 'red'}}>This page still a work in progress... (side projects while working can be really hard and demanding)</p>
                 <Timeline />
